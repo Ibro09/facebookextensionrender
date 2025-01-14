47d6679e7ad753fcf09d7464f99c084502111fc4
ibrahim <ibsalam24@gmail.com>
2025-01-14 04:39:13 +0100

    testing copy commit

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -18,21 +18,29 @@ const scrapeLogic = async (res) => {
   });
   try {
     const page = await browser.newPage();
-      const context = await browser.defaultBrowserContext();
-      await context.overridePermissions("https://web.facebook.com", [
-        "clipboard-read",
-        "clipboard-write",
-      ]);
+    const context = await browser.defaultBrowserContext();
+    await context.overridePermissions("https://web.facebook.com", [
+      "clipboard-read",
+      "clipboard-write",
+    ]);
+    await context._connection.send("Browser.grantPermissions", {
+      origin: url.origin,
+      browserContextId: this._id || undefined,
+      permissions: ["clipboardReadWrite", "clipboardSanitizedWrite"],
+    });
     await page.goto("https://developer.chrome.com/");
-     await page.evaluate(async () => {
-       await navigator.clipboard.writeText("Hello, Clipboard!");
-       const text = await navigator.clipboard.readText();
-       console.log(text); // Should log "Hello, Clipboard!"
-       res.send(text);
-     });
-  context.clearPermissionOverrides();
-
-
+    const state = await page.evaluate(async () => {
+      return (await navigator.permissions.query({ name: "clipboard-write" }))
+        .state;
+    });
+    console.log(state); // granted
+    await page.evaluate(async () => {
+      await navigator.clipboard.writeText("Hello, Clipboard!");
+      const text = await navigator.clipboard.readText();
+      console.log(text); // Should log "Hello, Clipboard!"
+      res.send(text);
+    });
+    context.clearPermissionOverrides();
   } catch (e) {
     console.error(e);
     res.send(`Something went wrong while running Puppeteer: ${e}`);
