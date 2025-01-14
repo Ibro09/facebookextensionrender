bd15a901785f1a612e27086325bb92e66f208001
ibrahim <ibsalam24@gmail.com>
2025-01-14 04:07:31 +0100

    initial commit

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -18,15 +18,20 @@ const scrapeLogic = async (res) => {
   });
   try {
     const page = await browser.newPage();
-
+      const context = browser.defaultBrowserContext();
+      await context.overridePermissions("https://developer.chrome.com/", [
+        "clipboard-read",
+        "clipboard-write",
+        "clipboard-sanitized-write",
+      ]);
     await page.goto("https://developer.chrome.com/");
      await page.evaluate(async () => {
        await navigator.clipboard.writeText("Hello, Clipboard!");
        const text = await navigator.clipboard.readText();
        console.log(text); // Should log "Hello, Clipboard!"
+       res.send(text);
      });
 
-    res.send('done');
   } catch (e) {
     console.error(e);
     res.send(`Something went wrong while running Puppeteer: ${e}`);
